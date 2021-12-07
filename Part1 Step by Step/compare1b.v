18bd8f6aab9c55d8d5ac3d1b0aee718e50ce8be6
颜琪滨 <2535367985@qq.com>
2021-12-08 00:01:26 +0800

    1.1 simple combination logic design

---
+++ Part1 Step by Step/compare1b.v
@@ -0,0 +1,12 @@
+// File: compare1b.v
+  `timescale 10 ns / 1 ns
+  
+  module compare1b(y, b, a);
+    
+    output y;   // ??????
+    input b, a;  // ??????
+    
+    // ?????????
+    assign y = (a==b) ? 1 : 0;
+    
+  endmodule
